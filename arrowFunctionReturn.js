546ff91106e13126fb766eea1ada6cf554689607
Sishir Pokhrel <pokhrelsishir@gmail.com>
2022-09-08 16:27:31 -0500

    Arrow Functions return statement examples

--- arrowFunctionReturn.js
+++ arrowFunctionReturn.js
@@ -13,6 +13,9 @@ let add2 = (a,b) =>
 // This can be again written as the below removing the return key word 
 let add3 = (a,b) => a+b;
 
+// But a point that needs to be noted is that if return is removed from an statement, parentheiss should be also removed or else we will get an error
+
+
 // This means a function takes parameters a and b and then return the value
 // Value after arrow is being returned
 
@@ -25,4 +28,21 @@ let sqr = (x) => x*x;
 
 let sqr2 = x => x*x;    // This is actually close to mathematics function eg: f:x => x^2
 
+
+
+/////////////////////////////////////////////////////////
+
+let greet2 = (firstname) => {
+    return `Hi ${firstname} `;
+}
+
+console.log(greet2('Tony'));
+
+
+// The above function can be replaced by the following arrow function
+
+
+let greet = firstname => `Hi ${firstname}`;
+
+console.log(greet('Alicia'));
 
